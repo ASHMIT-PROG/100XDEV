c65e784daa927e837a0ebcd459b73effe87fb612
Ashmit Sharma <thatcodingguy1616@gmail.com>
2025-12-17 20:24:35 +0530

    ES-6 MODULE

---
+++ WEB DEV/WEEK 4/NODE.JS - EXTENDED/common_js_modules._1.mjs
@@ -0,0 +1,2 @@
+const simple = require ("./common_js_modules_2.mjs")// simple function ko maine import kiya , ./ se woh file jisse mai import krr raha hoon
+simple()// abb import ho gaya , toh run krr dunga 
---
+++ WEB DEV/WEEK 4/NODE.JS - EXTENDED/common_js_modules_2.mjs
@@ -0,0 +1,6 @@
+function simple(){
+    console.log("just keep it simple ");
+    
+}
+
+module.exports = simple// isse mai simple function ko export krr sakta hoon kisi bhi file me 
