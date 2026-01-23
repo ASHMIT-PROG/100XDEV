15e21e5d6a64e0dc8003ee00e6818f9a559eaaad
ASHMIT-PROG <thatcodingguy1616@gmail.com>
2026-01-24 00:17:14 +0530

    did some changes"

--- WEB DEV/WEEK 5/WEEK 5.1/MAKING_HTTP_SERVER.js
+++ WEB DEV/WEEK 5/WEEK 5.1/MAKING_HTTP_SERVER.js
@@ -1,14 +1,21 @@
+//creating an http server that have 4 routes
+/*1. http://localhost:3000/multiply?a=1&b=2
+2. [http://localhost:3000/add?a=1&b=2](http://localhost:3000/multiply?a=1&b=2)
+3. [http://localhost:3000/divide?a=1&b=2](http://localhost:3000/multiply?a=1&b=2)
+4. [http://localhost:3000/subtract?a=1&b=2](http://localhost:3000/multiply?a=1&b=2) */
+
+
 const express = require("express");
 const app = express()
 // sum function 
-app.get("/sum",function(req,res){
-    const a = parseInt (req.query.a)// agar parseInt nahi lagata toh ye string value deta 
-    const b = parseInt (req.query.b)
+app.get("/sum/:a/:b",function(req,res){ // "/sum" is an end point , 
+    const a = parseInt (req.params.a)// agar parseInt nahi lagata toh ye string value deta 
+    const b = parseInt (req.params.b)
 
     res.json({ //ek function ke andar object pass kita  hai 
         ans:a+b
     })
-})
+})// http://localhost:3000/sum/32/1234
 
 // sub function     
 app.get("/sub",function(req,res){
